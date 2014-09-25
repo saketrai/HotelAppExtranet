41a3b4456f4639ebe10aa7d451b1a5bff215eda4
Saket <saketrai@gmail.com>
2014-09-24 22:34:43 -0700

    First commit

--- HotelExtranet/AppDelegate.m
+++ HotelExtranet/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"1wsLowwywGjGu8mexlVioLhmx2Hz4ufntY8ui4Ve"
+                  clientKey:@"hlxDUmxadGWUb9d9SfgyOGMoPzwzgzrB5ksQvTKu"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
